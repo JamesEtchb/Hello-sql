4c0b760c85dc0a584dc85c5f2fcb6276121d154c
James Etchberger <jamesetchberger@Jamess-MacBook-Air.local>
2022-07-20 16:46:21 -0400

    made a js file that calls on the database we made earlier

---
+++ index.js
@@ -0,0 +1,20 @@
+//import the mysql to library
+const mysql = require("mysql2")
+
+//connect to our database
+const db = mysql.createConnection({
+  host: "34.170.135.140",
+  user: "root",
+  password: "BuildingSoFLo",
+  database: "swecc-summer-2022",
+})
+
+//run a simple query
+db.query("SELECT * FROM Books_je", (err, results) => {
+  if (err) {
+    console.log(err)
+  }
+  //output the results
+  console.log(results)
+  db.end()
+})
